2eb167173e15e69c30c63b4a3964e5fbc8c7227d
Methma Abeyrathna <methmasasmini02@gmail.com>
2025-02-19 09:27:22 +0530

    2025-02-19

--- RuchiToursWPF/Home.xaml.cs
+++ RuchiToursWPF/Home.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using static Google.Protobuf.Reflection.SourceCodeInfo.Types;
 
 namespace RuchiToursWPF
 {
@@ -22,6 +23,39 @@ namespace RuchiToursWPF
         public Home()
         {
             InitializeComponent();
+        }
+
+        private void BtnLogout_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult check = MessageBox.Show("Are You sure you want to logout?"
+                ,"Confirmation Message",MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (check == MessageBoxResult.Yes)
+            {
+                MainWindow loginForm = new MainWindow();
+                loginForm.Show();
+                this.Hide();
+            }
+        }
+
+        private void BtnAddNewTour_Click(object sender, RoutedEventArgs e)
+        {
+           
+            viewTours.IsVisible = false;
+            guestDetails1.Visible = false;
+            drivers1.Visible = false;
+            guides1.Visible = false;
+            vehicle1.Visible = false;
+            currency1.Visible = false;
+            locations1.Visible = false;
+            accommodations1.Visible = false;
+            addAccommodation1.Visible = false;
+            addLocations1.Visible = false;
+            addCurrency1.Visible = false;
+            addVehicles1.Visible = false;
+            addGuides1.Visible = false;
+            addDrivers1.Visible = false;
+            addGuestDetails1.Visible = false;
         }
     }
 }
